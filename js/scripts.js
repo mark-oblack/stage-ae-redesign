ddc958bb25724c5c78d9bd4994b43656c4f1a70a
mark-oblack <moblack42@gmail.com>
2020-05-02 11:06:07 -0400

    adjust nav toggle icon, changes src on click

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,12 @@ function responsiveNav() {
 	if (x.className === "topnav" && y.className === "topnav-items") {
 	  x.className += " responsive";
 	  y.className += " responsive";
+	  document.getElementById("nav-toggle").src="assets/icons/close-w.svg";
 	} else {
 	  x.className = "topnav";
 	  y.className = "topnav-items";
+	  document.getElementById("nav-toggle").classList.remove('fa-times');
+	  document.getElementById("nav-toggle").classList.add('fa-bars');
+	  document.getElementById("nav-toggle").src="assets/icons/hamburger-w.svg";
 	}
 }
-
-function openNav() {
-    document.getElementById("myTopnav").style.width = "300px";
-  }
-  
-  function closeNav() {
-    document.getElementById("myTopnav").style.width = "0";
-  }
